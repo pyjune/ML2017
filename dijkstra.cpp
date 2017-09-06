2576955668320ef1b184a8e8896e7faa67539f07
pyjune <pyjune@naver.com>
2017-09-06 10:54:57 +0900

    Update dijkstra.cpp

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -8,15 +8,19 @@ int adj[11][11];
 int V, E;
 
 void dij(void);
+void bfs(void);
+int Q[100];
+int front = -1;
+int rear = -1;
 
 int main(void)
 {
 	freopen("Text.txt", "r", stdin);
 	scanf("%d %d", &V, &E);
 
-	for (int i = 1; i <= V; i++)
+	for (int i = 0; i <= V; i++)
 	{
-		for (int j = 1; j <= V; j++)
+		for (int j = 0; j <= V; j++)
 		{
 			if (i == j)
 				adj[i][j] = 0;
@@ -31,7 +35,8 @@ int main(void)
 		scanf("%d %d %d", &n1, &n2, &w);
 		adj[n1][n2] = w;
 	}
-	dij();
+	//dij();
+	bfs();
 	printf("%d\n", d[V]);
 }
 
@@ -64,5 +69,36 @@ void dij(void)
 			}
 		}
 		cnt++;
+	}
+}
+
+
+void bfs(void)
+{
+	// 시작점의 인접 노드를 enqueue, d[] 초기화
+	for (int i = 0; i <= V; i++)
+	{
+		if (adj[0][i] != 0 && adj[0][i] != INF)
+		{
+			Q[++rear] = i;
+		}
+		d[i] = adj[0][i];
+	}
+	while (front != rear)
+	{
+		int t = Q[++front];
+		// t를 경유지로. t와 인접인 모든 i에 대해...
+		for (int i = 0; i <= V; i++)
+		{
+			if (adj[t][i] != 0 && adj[t][i] != INF)
+			{
+				if (d[i] > (d[t] + adj[t][i])) // 갱신이 되면
+				{
+					d[i] = d[t] + adj[t][i];
+					Q[++rear] = i;  // 갱신된 i의 인접도 갱신될 수 있으므로 enqueue
+				}
+
+			}
+		}
 	}
 }
